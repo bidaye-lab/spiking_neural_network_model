f8a788add98aa3e22e776cfe64e6f829b1d52a88
Nico Spiller <nspiller@mailbox.org>
2023-03-01 15:41:22 -0500

    wrapper for name2flyid dict

--- utils.py
+++ utils.py
@@ -1,153 +1,171 @@
 import pandas as pd
-import numpy as np
 from pathlib import Path
-import pickle
 
-# load from disk
-def load_dfs(path_comp, path_con):
-    
-    # load neuron data
-    df_comp = pd.read_csv(path_comp, index_col = 0) # neuron ids and excitation type
-    df_con = pd.read_csv(path_con, index_col = 0) # connectivity
-
-    return df_comp, df_con
-
-def load_dicts(path_name):
-
-    # load name mappings
-    with open(path_name, 'rb') as f:
-        flyid2i, flyid2name, i2flyid, i2name, name2flyid, name2i = pickle.load(f)
-
-    return flyid2i, flyid2name, i2flyid, i2name, name2flyid, name2i 
-
-def load_names(path_name):
-
-    path_name = Path(path_name)
-    flyid2i, flyid2name, i2flyid, i2name, name2flyid, name2i = load_dicts(path_name)
-    names = [ *name2flyid.keys() ]
-    return names
-
-def load_exps(path_res, exp_type):
-    '''Load simulation results from disk
+def load_xls(path_names, sheets_pair, sheets_single):
+    '''Load XLS file containing neuron name to flywire ID mapping
 
     Parameters
     ----------
-    path_res : str
-        Path to results folder
-    exp_type : str
-        type of the experiment (coac | dly)
+    path_names : str
+        Path to XLS file
+    sheets_pair : list
+        List of sheet names containing neuron pairs
+    sheets_single : list
+        List of sheet names containing individual neurons
 
     Returns
     -------
-    exps : dict
-        data for all experiments of 'exp_type' in 'path_res'
+    df_pair : pd.DataFrame
+        Dataframe mirroring the pair sheets
+    df_sing : pd.DataFrame
+        Dataframe mirroring the single sheets
     '''
-    # wildcard matching all outputs of 'exp_type'
-    pkl_glob = Path(path_res).glob('{}*.pickle'.format(exp_type))
-
-    # initilize dicts
-    exps = dict()
-
-    # cycle through all experiments
-    for p in pkl_glob:
-
-        # load metadata from pickle
-        with open(p, 'rb') as f:
-            pkl = pickle.load(f) 
-        n = pkl['exp_name']
-        exps[n] = pkl
-
-        # load spike times from feather
-        f = p.with_suffix('.feather')
-        exps[n]['data'] = pd.read_feather(f)
-
-    return exps
-
-# dataframe handling
-def collect_spikes(exps):
-
-    # collect spike times of all experiments in one dataframe
-    df = pd.DataFrame()
-
-    for e in exps: # cycle through experiments
-        df_exp = exps[e]['data'] # df for individual experiment
-        
-        #  sum up spike events
-        df_exp = df_exp.dropna(how='all') # select only rows containing spikes
-        ds = df_exp.apply(lambda x: [i for i in x], axis=1) # concatenate spk times, ignore nan
-        ds.name = e # dataseries name will convert to dataframe column
-        df = pd.concat([df, ds], axis=1)
     
-    return df
+    # sheets with left/right pairs (name | ID left | ID right)
+    dfs_pair = pd.read_excel( 
+        Path(path_names),
+        sheet_name=sheets_pair,
+        dtype={'ID_left': str, 'ID_right': str}
+        )
+    df_pair = pd.concat(dfs_pair, ignore_index=True).dropna(how='all')
 
-def rename_neurons(df, i2flyid, flyid2name):
+    # sheets with single neurons (name | ID)
+    dfs_single = pd.read_excel( 
+        Path(path_names),
+        sheet_name=sheets_single,
+        dtype={'ID': str}
+        )
+    df_single = pd.concat(dfs_single, ignore_index=True).dropna(how='all')
 
-    # rename brian ids to flywire/custom names
-    df = df.rename(index=i2flyid).rename(index=flyid2name) # (1) flywire ids and (2) custom names
-    df.index = df.index.astype(str) # represent flywire IDs as str, not int
+    # print info
+    print('INFO: Loaded sheets ...')
+    for i in [*dfs_pair.keys(), *dfs_single.keys()]:
+        print('      ... {}'.format(i))
+    print()
+
+    return df_pair, df_single
+
+def check_unique(df_pair, df_single):
+    '''Check for double definition of neuron names and flywire IDs.
+    Prints warning if duplicates found
+
+    Parameters
+    ----------
+    df_pair : pd.DataFrame
+        Names and flywire IDs for neuron pairs
+    df_single : pd.DataFrame
+        Names and flywire IDs for single neurons
+    '''
+
+    # check names
+    ds = pd.concat( (df_single.loc[:, 'Name'], df_pair.loc[:, 'Name']), ignore_index=True) # merge names from both dataframes
+    dup = ds.loc[ ds.duplicated(keep=False )] # series with duplicate values
+    if dup.empty:
+        print('INFO: All names are unique')
+    else:
+        print('WARNING: Found duplicate names:')
+        print(dup)
+    print()
+
+    # check flywire IDs
+    ds = pd.concat( (df_single.loc[:, 'ID'], df_pair.loc[:, 'ID_left'], df_pair.loc[:, 'ID_right']), ignore_index=True) # merge IDs from both dataframes
+    ds = ds.dropna() # igrone nan for IDs
+    dup = ds.loc[ ds.duplicated(keep=False )] # series with duplicate values
+    if dup.empty:
+        print('INFO: All IDs are unique')
+    else:
+        print('WARNING: Found duplicate IDs:')
+        ds_n = pd.concat( (df_single.loc[:, 'Name'], df_pair.loc[:, 'Name'], df_pair.loc[:, 'Name']), ignore_index=True) # series with names of same structure as ds
+        print(pd.concat( (ds_n.loc[dup.index], dup), axis=1 ) ) # pring names and IDs
+    print()
+
+def assign(name2id, name, id):
+    '''Assign custom name to flywire ID
+    Prints warning if flywire ID could not be interpreted as integer and skips those
+
+    Parameters
+    ----------
+    name2id : dict
+        dictionary to which to assign to
+    name : str
+        custom neuron name
+    id : str
+        str to be interpreted as flywired ID
+    '''
+    try:
+        name2id[name] = int(id)
+    except ValueError:
+        print('WARNING: Could not assign ID {} to name {}'.format(id, name))
+
+
+def check_ids(name2flyid, path_comp):
+    '''Check if flywire IDs of custom names appear in completeness file
+
+    Parameters
+    ----------
+    name2flyid : dict
+        Mapping between custom names and flywire IDs
+    path_comp : str
+        Path to completeness dataframe
+    '''
+
+    # check if IDs are correct: if everything is correct, nothing is printed
+    df_comp = pd.read_csv(Path(path_comp), index_col=0) # df with all IDs
+    ids_all = df_comp.index # all flywire ids
+
+    warn = False
+    for k, v in name2flyid.items():
+        if not v in ids_all:
+            print('WARNING: ID {} for neuron {} not found'.format(str(v), k))
+            warn = True
+    if not warn:
+        print('INFO: IDs appear to match with {}'.format(path_comp))
+    print()
+  
+
+def create_name_dict(path_name, path_comp, sheets_pair, sheets_single):
+    '''Generate dictionary mapping custom neuron names to flywire IDs
+
+    Parameters
+    ----------
+    path_name : str
+        Path to XLS files containing neuron names and flywire IDs
+    path_comp : str
+        Path to completeness dataframe
+    sheets_pair : list
+        List of sheet names containing neuron pairs
+    sheets_single : list
+        List of sheet names containing individual neurons
+
+    Returns
+    -------
+    name2flyid : dict
+        Mapping between custom neuron names to flywire IDs
+    '''
+
+    # load XLS file into dataframes
+    df_pair, df_single = load_xls(path_name, sheets_pair, sheets_single)
     
-    # sort indixes: named first
-    idx_n = [ i for i in flyid2name.values() if i in df.index] # named neurons
-    idx_i = [ i for i in df.index if i not in flyid2name.values() ] # flywire ids
-    idx_i.sort() # sort flywire ids
-    df = df.loc[idx_n + idx_i, :] # first named neurons, then flywire ids
+    # check for duplicates
+    check_unique(df_pair, df_single)
 
-    return df
-    
-def calculate_rates(df, n_runs=30, t_sim=1):
-    
-    def get_count(x):
-        n = 0
-        for i in x:
-            if i is not None:
-                n += len(i)
-        n 
-        return n
+    # create dictionary mapping custom names to flywire IDs
+    name2flyid = dict()
 
-    df_count = df.applymap(get_count, na_action='ignore') # count spike events
-    df_rate = df_count / ( n_runs * t_sim )
+    for i in df_pair.index: # left/right pairs
+        n, id_l, id_r = df_pair.loc[i, ['Name', 'ID_left', 'ID_right']]
+        n_l, n_r = '{}_l'.format(n), '{}_r'.format(n)
+        assign(name2flyid, n_l, id_l)
+        assign(name2flyid, n_r, id_r)
 
-    df_rate = df_rate.fillna(0) # replace nan with 0, necessary for differences later
+    for i in df_single.index: # single neurons
+        n, id = df_single.loc[i, ['Name', 'ID']]
+        assign(name2flyid, n, id)    
 
-    return df_rate
+    print( 'Declared {} names for neurons'.format(len(name2flyid)))
+    print()
 
-def save_all_xls(df, file):
-    # save as xlsx
-    with pd.ExcelWriter(file, mode='w', engine='xlsxwriter') as w:
-        df.to_excel(w, sheet_name='all_experiments')
+    # check if all flywire IDs appar in neurotransmitter data
+    check_ids(name2flyid, path_comp)
 
-        # formatting in the xlsx file
-        wb = w.book
-        # set floating point display precision here (excel format)
-        fmt = wb.add_format({'num_format': '#,##0.0'}) 
-        for _, ws in w.sheets.items():
-            ws.set_column(1, 1, 10, fmt)
-            ws.freeze_panes(1, 1)
-
-# plotting
-def plot_raster(df, exps, neurons, xlims=(0, 2), path=None):
-    n_exp, n_neu = len(exps), len(neurons)
-    fig, axmat = plt.subplots(ncols=n_neu, nrows=n_exp, squeeze=False, figsize=(3*n_neu, 2*n_exp))
-    for i, e in enumerate(exps):
-        for j, n in enumerate(neurons):
-            ax = axmat[i,j]
-            if j == 0:
-                ax.set_ylabel(e)
-            if i == 0:
-                ax.set_title(n)
-            trials = df.loc[n, e]
-            if type(trials) == list:
-                for k, t in enumerate(trials):
-                    if str(type(t)) != str(type(None)):
-                        ax.eventplot(t, lineoffset=k, linewidths=.5)
-                ax.set_ylim(-0.5, len(trials)-0.5)
-            ax.grid(None)
-            ax.set_xlim(xlims)
-            ax.set_yticklabels('')
-    for ax in axmat[-1]:
-        ax.set_xlabel('time [s]')
-
-    fig.tight_layout()
-
-    if path:
-        fig.savefig(path)
+    return name2flyid
